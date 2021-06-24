a35b7ae0d409e4e14696e771c4171da1bda9f48a
piekstra <piekstra@users.noreply.github.com>
2021-06-23 17:46:58 -0700

    Add placeholder tests

--- tests/test_insights_client.py
+++ tests/test_insights_client.py
@@ -1,3 +1,17 @@
+import pytest
 # from newrelic import NewRelicInsightsApi
 
+
 # TODO - This needs tests!
+@pytest.fixture(scope='module')
+def client():
+    return None
+    # return NewRelicInsightsApi(account_id, api_key)
+
+
+@pytest.mark.usefixtures('client')
+class TestClient(object):
+
+    def test(self, client):
+        # TODO!
+        assert True
